3c253f9631dc469e8a8b2acc57da007c4dc81763
trinhngoc2709 <trinhbaongoc2709@gmail.com>
2021-07-03 22:51:30 +0700

    complete error list

--- MyWebsite/js/base.js
+++ MyWebsite/js/base.js
@@ -2,8 +2,8 @@
     /***
      Created by tbNgoc at 30/06/2021
      * **/
-     constructor() {
-
+    constructor() {
+      
     }
     /***
      Created by tbNgoc at 30/06/2021
@@ -19,7 +19,7 @@
      Check valid number
      * **/
     checkValidNumber(str) {
-        return /^\d+$/.test(str.replaceAll('.', '')) || "" == str;
+        return /^\d+$/.test(str.replaceAll('.', ''));
     }
     /***
      Created by tbNgoc at 30/06/2021
@@ -36,24 +36,41 @@
      Created by tbNgoc at 30/06/2021
      Check Tax Number
      * **/
-    checkTaxNumber(taxString) {
-        if (taxString) {
-            let toNumberTax = parseInt(taxString, 10);
-            if (String(taxString).length != 10 || (!this.checkValidNumber(taxString))
-                || (toNumberTax < 1 || toNumberTax > 9999999999 || toNumberTax == 9999999000))
-                return false;
-            else return true;
-        } else return false;
+    checkTaxNumber(taxString,objScope) {
+        if(this){
+            if (taxString) {
+                let toNumberTax = parseInt(taxString, 10);
+                if (String(taxString).length != 10 || (!this.checkValidNumber(taxString))
+                    || (toNumberTax < 1 || toNumberTax > 9999999999 || toNumberTax == 9999999000))
+                    return false;
+                else return true;
+            } else return false;
+        }else{
+            if (taxString) {
+                let toNumberTax = parseInt(taxString, 10);
+                if (String(taxString).length != 10 || (!objScope.checkValidNumber(taxString))
+                    || (toNumberTax < 1 || toNumberTax > 9999999999 || toNumberTax == 9999999000))
+                    return false;
+                else return true;
+            } else return false;
+        }
     }
     /***
      Created by tbNgoc at 30/06/2021
      Check identity number
      * **/
-    checkIdNumber(idString) {
-        console.log(idString.length);
-        if (idString && this.checkValidNumber(idString) && (idString.length == 9 || idString.length == 12)) {
-            return true;
-        } return false;
+    checkIdNumber(idString,objScope) {
+        console.log(objScope);
+        if(this){
+            if (idString && this.checkValidNumber(idString) && (idString.length == 9 || idString.length == 12)) {
+                return true;
+            } return false;
+        }else{
+            if (idString && objScope.checkValidNumber(idString) && (idString.length == 9 || idString.length == 12)) {
+                return true;
+            } return false;
+        }
+        
     }
     /***
      Created by tbNgoc at 30/06/2021
@@ -155,7 +172,6 @@
         function displayOption(options, keyCode, inputField) {
             let currentIndex = 0;
             let maxIndex = options.length;
-            console.log(typeof (options));
             for (const option of options) {
                 if (option.className == "active-option") {
                     currentIndex = Array.prototype.indexOf.call(options, option)
@@ -191,20 +207,32 @@
             document.querySelector('.employee-detail .employee-detail-content input').click();
         });
         //Event for save button
+        // Close the error Dialog
+        let closeButtonLayout1 = document.querySelector('.popup-list .popup-footer button');
+        closeButtonLayout1.addEventListener('click', () => {
+            let popupLayout = document.querySelector('.popup-layout');
+            let errorListDialog = document.querySelector('.popup-list');
+            popupLayout.style.width = "0px";
+            errorListDialog.style.width = "0px";
+        })
+        // Close the error1 
+        let closeButtonLayout2 = document.querySelector('.popup-list .close-button');
+        closeButtonLayout2.addEventListener('click', () => {
+            let popupLayout = document.querySelector('.popup-layout');
+            let errorListDialog = document.querySelector('.popup-list');
+            popupLayout.style.width = "0px";
+            errorListDialog.style.width = "0px";
+        })
+        // Event click
         document.querySelector('#btn-save').addEventListener("click", () => {
-            if (checkValidInputs()) {
+            if (checkValidInputs(this)) {
                 alert("Lưu thành công");
+                initCustomizedSelect();
             } else {
                 let popupLayout = document.querySelector('.popup-layout');
                 popupLayout.style.width = "100vw";
-                let closeButtonLayout = document.querySelector('.popup-footer button');
                 let errorListDialog = document.querySelector('.popup-list');
                 errorListDialog.style.width = "500px";
-                errorListDialog.removeEventListener('click', true);
-                closeButtonLayout.addEventListener('click', () => {
-                    popupLayout.style.width = "0px";
-                    errorListDialog.style.width = "0px";
-                })
             }
         });
         // Function check Error Input Field and display error
@@ -214,15 +242,16 @@
             "tax": this.checkTaxNumber,
             "id": this.checkIdNumber
         }
-        function checkValidInputs() {
+        function checkValidInputs(objScope) {
             let check = true;
             let content = "";
             let errorBlockContent = document.querySelector('.error-content');
+            errorBlockContent.innerHTML = "";
             // Check required field
             let requiredFields = document.querySelectorAll('.input-information .user-input.required');
             requiredFields.forEach(element => {
                 if (element.value == "") {
-                    content = "Thiếu trường \"" + element.parentElement.childNodes[0].innerText + "\"";
+                    content = "Thiếu trường \"" + element.parentElement.querySelector('.name-information').innerText + "\"";
                     errorBlockContent.appendChild(createErrorContent(content));
                     check = false;
                 }
@@ -232,27 +261,27 @@
             userInputs.forEach(element => {
                 let classListElement = element.classList;
                 if (classListElement.contains("number-input")) {
-                    content = "Thông tin số điện thoại bị nhập sai";
+                    content = "Thông tin \"số điện thoại\" bị nhập sai";
                     checkValidInput(content, validateFunctions.number, element.value, errorBlockContent) ? true : check = false;
                 } else if (classListElement.contains("email-input")) {
-                    content = "Thông tin email bị nhập sai";
+                    content = "Thông tin \"email\" bị nhập sai";
                     checkValidInput(content, validateFunctions.email, element.value, errorBlockContent) ? true : check = false;
                 } else if (classListElement.contains("tax-input")) {
-                    content = "Thông tin mã số thuế bị nhập sai";
-                    checkValidInput(content, validateFunctions.tax, element.value, errorBlockContent) ? true : check = false;
+                    content = "Thông tin \"mã số thuế\" bị nhập sai";
+                    checkValidInput(content, validateFunctions.tax, element.value, errorBlockContent,objScope) ? true : check = false;
                 } else if (classListElement.contains("input-currency")) {
-                    content = "Thông tin lương cơ bản bị nhập sai";
+                    content = "Thông tin \"lương cơ bản\" bị nhập sai";
                     checkValidInput(content, validateFunctions.number, element.value.replaceAll('.', ''), errorBlockContent) ? true : check = false;
                 } else if (classListElement.contains("id-input")) {
-                    content = "Thông tin CMND bị nhập sai";
-                    checkValidInput(content, validateFunctions.id, element.value, errorBlockContent) ? true : check = false;
+                    content = "Thông tin \"CMND\" bị nhập sai";
+                    checkValidInput(content, validateFunctions.id, element.value, errorBlockContent,objScope) ? true : check = false;
                 }
             })
             // check date picker field
             let datePickerField = document.querySelectorAll('.input-information .user-input[type="date"]');
             datePickerField.forEach(element => {
                 if (element.value == "") {
-                    content = "Thiếu trường \"" + element.parentElement.childNodes[0].innerText + "\"";
+                    content = "Thiếu trường \"" + element.parentElement.querySelector('.name-information').innerText + "\"";
                     errorBlockContent.appendChild(createErrorContent(content));
                     check = false;
                 }
@@ -260,11 +289,11 @@
             return check;
         }
         // Check valid and add Error Block 
-        function checkValidInput(content, functionValidate, valueCheck ,errorBlockContent) {
-            if (!functionValidate(valueCheck)) {
+        function checkValidInput(content, functionValidate, valueCheck ,errorBlockContent,objScope) {
+            if (!functionValidate(valueCheck,objScope)) {
                 errorBlockContent.appendChild(createErrorContent(content));
                 return false;
-            }
+            } return true;
         }
         // Create error content
         function createErrorContent(content) {
@@ -290,8 +319,8 @@
         // Event for close popup
         let closeButtonLayout = document.querySelector('.popup-footer button:last-of-type');
         
-        let cancleButtonLayout = document.querySelector('.popup-footer button:first-of-type');
-        let cancleButtonLayout1 = document.querySelector('.popup-warning .close-button');
+        let cancelButtonLayout = document.querySelector('.popup-footer button:first-of-type');
+        let cancelButtonLayout1 = document.querySelector('.popup-warning .close-button');
         // Close employee detail form
         closeButtonLayout.addEventListener("click", () => {
             document.querySelector('#upload-img').src = defaultAvatar;
@@ -302,11 +331,11 @@
             document.querySelector('.popup-warning').style.width = "0px";
         })
         // Continue input detail form
-        cancleButtonLayout.addEventListener("click", () => {
+        cancelButtonLayout.addEventListener("click", () => {
             document.querySelector('.popup-layout').style.width = "0px";
             document.querySelector('.popup-warning').style.width = "0px";
         })
-        cancleButtonLayout1.addEventListener("click", () => {
+        cancelButtonLayout1.addEventListener("click", () => {
             document.querySelector('.popup-layout').style.width = "0px";
             document.querySelector('.popup-warning').style.width = "0px";
         })
@@ -323,7 +352,6 @@
                 }
                 if (((e.keyCode >= 96 && e.keyCode <= 105) || (e.keyCode >= 48 && e.keyCode <= 57)) && element.classList.contains("input-currency"))
                     element.value = this.formatMoney(element.value);
-
             });
             // Event focus for element
             focusEvent(element, this.checkValidNumber);
@@ -347,17 +375,17 @@
                 removeError(e.target);
             }
         });
-        focusEvent(idInput, this.checkIdNumber);
+        focusEvent(idInput, this.checkIdNumber,this);
         // Check valid tax number input field
         let taxInput = document.querySelector('.tax-input');
         taxInput.addEventListener("keyup", (e) => {
-            if (!this.checkTaxNumber(e.target.value)) {
+            if (!this.checkTaxNumber(e.target.value) && e.target.value != "") {
                 displayError(e.target);
             } else {
                 removeError(e.target);
             }
         });
-        focusEvent(taxInput, this.checkTaxNumber);
+        focusEvent(taxInput, this.checkTaxNumber,this);
         // Function display error
         function displayError(element) {
             element.style.borderColor = "#FF4747";
@@ -375,14 +403,14 @@
                 element.parentElement.querySelector(".wrong-input").remove();
         }
         // Focus on, focus out event
-        function focusEvent(e, functionCheck) {
+        function focusEvent(e, functionCheck,objScope) {
             e.addEventListener("focusout", (e1) => {
                 if (e1.target.parentElement.querySelector(".wrong-input") == undefined)
                     e1.target.style.borderColor = "#bbbbbb";
             })
             e.addEventListener("focusin", (e1) => {
                 e1.target.style.borderColor = "#019160";
-                if (!functionCheck(e1.target.value) && e1.target.value != "") {
+                if (!functionCheck(e1.target.value,objScope) && e1.target.value != "") {
                     displayError(e1.target);
                     e1.target.style.borderColor = "#FF4747";
                 }
